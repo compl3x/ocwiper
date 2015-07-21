0ed0f081b8b35b98b8838bd1de2d3458c6d59109
Michael Cowell <michael.cowell3@gmail.com>
2015-07-22 00:54:56 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "cwipe compile script v0.1"
 echo "GCC Version: $(gcc -dumpversion)"
-echo "Output: $(pwd)/out/wipe"
+echo "Output: $(pwd)/out/ocwiper"
 mkdir -p out && gcc -o ./out/ocwiper wipe.c mtwist.c
